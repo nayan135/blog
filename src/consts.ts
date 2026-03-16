8196c28b8321a3e4afec2c08e957f04505664243
NAYAN ACHARYA <100992818+nayan135@users.noreply.github.com>
2026-03-16 07:12:50 +0545

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'NAYAN ACHARYA - WEb | STUDENT | BLOG';
+export const SITE_TITLE = 'NAYAN ACHARYA - WEB | STUDENT | BLOG';
 export const SITE_DESCRIPTION = "NAYAN135 is a person who does something even he doesn't know what he is doing";
+
